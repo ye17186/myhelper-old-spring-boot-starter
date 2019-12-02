d62f1197a64b21d47040ad3e8cbdf1189c848fbf
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-12-02 15:39:52 +0800

    springboot版本升级到2.2.1

--- src/main/java/com/yclouds/myhelper/feign/fallback/AbstractFeignFallback.java
+++ src/main/java/com/yclouds/myhelper/feign/fallback/AbstractFeignFallback.java
@@ -5,6 +5,7 @@ import com.yclouds.myhelper.web.response.ApiResp;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * AbstractFallback
@@ -17,7 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 public abstract class AbstractFeignFallback {
 
-    private String serviceId = "unknown";
+    @Value("${spring.application.name:unknown}")
+    private String serviceId;
 
     /**
      * Feign发生fallback时错误日志打印
@@ -25,7 +27,7 @@ public abstract class AbstractFeignFallback {
      * @param cause 错误异常
      */
     protected void printLog(Throwable cause) {
-        log.error("[{}] fallback.", serviceId, cause);
+        log.error("服务[{}] fallback.", serviceId, cause);
     }
 
     /**
@@ -38,8 +40,18 @@ public abstract class AbstractFeignFallback {
     protected <T> ApiResp<T> fallback(String method) {
 
         return ApiResp.retFail(BaseEnumError.SERVICE_DOWN.getCode(),
-            BaseEnumError.SERVICE_DOWN.getMsg().replace("${serviceId}", serviceId + "." + method));
+            BaseEnumError.SERVICE_DOWN.getMsg().replace("${serviceId}", serviceId + "#" + method));
     }
 
+    /**
+     * Feign发生fallback时的处理
+     *
+     * @param <T> 响应体泛型
+     * @return 具体响应
+     */
+    protected <T> ApiResp<T> fallback() {
 
+        return ApiResp.retFail(BaseEnumError.SERVICE_DOWN.getCode(),
+            BaseEnumError.SERVICE_DOWN.getMsg().replace("${serviceId}", serviceId));
+    }
 }
